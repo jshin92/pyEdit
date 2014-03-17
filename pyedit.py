75a9137babe0c03f59dc3912873b215a12969013
joe <joe@belgarath>
2014-03-17 15:12:57 -0700

    can write on top line

--- pyedit.py
+++ pyedit.py
@@ -10,6 +10,9 @@ pygame.init()
 screen = pygame.display.set_mode([WIDTH, HEIGHT])
 pygame.display.set_caption("pyEdit")
 
+text = [""]
+font = pygame.font.SysFont("monospace", 15)
+
 done = False
 clock = pygame.time.Clock()
 
@@ -17,9 +20,15 @@ while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
+        elif event.type == pygame.KEYDOWN:
+            text[0] = text[0] + event.unicode
+            print(event.unicode)
+
 
     screen.fill(WHITE)
 
+    label = font.render(text[0], 1, BLACK)
+    screen.blit(label, (0, 0))
     pygame.display.flip()
 
     clock.tick(FPS)
