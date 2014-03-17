6b70122eca4863db4c2532140b0b2fa742fc6ac3
joe <joe@belgarath>
2014-03-17 16:04:35 -0700

    allow for arbitrary hold

--- pyedit.py
+++ pyedit.py
@@ -5,14 +5,14 @@ WIDTH = 680
 HEIGHT = 320
 FPS = 60
 FONT_SIZE = 24
-BACKSPACE_THRESHOLD = 15
+KEY_THRESHOLD = 15
 text = [""]
 curLine = 0
 
 # todo:
 # 1) cursor
-# 2) allow for holding any key arbitrary length
-# 3) add camera for moving type off screen
+# 2) add camera for moving type off screen
+
 
 def main():
     global text
@@ -23,8 +23,9 @@ def main():
 
     font = pygame.font.SysFont("consolas", FONT_SIZE)
 
-    backspace_ticks = 0
+    cur_ticks = 0
     done = False
+
     clock = pygame.time.Clock()
 
     while not done:
@@ -32,28 +33,30 @@ def main():
             if event.type == pygame.QUIT:
                 done = True
             elif event.type == pygame.KEYDOWN:
+                cur_ticks = 1
+                cur_key = event.key
                 if event.key == pygame.K_TAB:
                     text[curLine] += '    '
                 elif event.key == pygame.K_RETURN:
                     text.append('')
                     curLine += 1
                 elif event.key == pygame.K_BACKSPACE:
-                    print(event.key)
                     handle_backspace()
-                    backspace_ticks = 1
                 else:
                     text[curLine] += event.unicode
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_BACKSPACE:
-                    backspace_ticks = 0
+                cur_ticks = 0
 
         screen.fill(WHITE)
 
-        if backspace_ticks > 0:
-            backspace_ticks += 1
+        if cur_ticks > 0:
+            cur_ticks += 1
 
-        if backspace_ticks > BACKSPACE_THRESHOLD:
-            handle_backspace()
+        if cur_ticks > KEY_THRESHOLD:
+            if cur_key == pygame.K_BACKSPACE:
+                handle_backspace()
+            else:
+                text[curLine] += event.unicode
 
         for i in range(curLine + 1):
             label = font.render(text[i], 1, BLACK)
